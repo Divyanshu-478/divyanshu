fd9fc7d3129e4942a226dc951e9c4bdad9f863bc
Divyanshu-478 <divyanshuaind20@gmail.com>
2026-09-05 00:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> suffix(n, 0);
 
-        int mn = INT_MAX; 
-        // Build suffix minimum
-        for (int i = n - 1; i >= 0; i--){
-            mn = min(mn, nums[i]);
-            suffix[i] = mn;
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; --i) {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
         }
- 
-        int mx = 0;
-        // Find first index with score <= k
-        for (int i = 0; i < n; i++){
-            mx = max(mx, nums[i]);
-            int score = mx - suffix[i];
-            if (score <= k) return i;
+
+        int prefixMax = nums[0];
+
+        for (int i = 0; i < n; ++i) {
+            prefixMax = max(prefixMax, nums[i]);
+
+            int instability = prefixMax - suffixMin[i];
+
+            if (instability <= k) {
+                return i;
+            }
         }
- 
+
         return -1;
     }
 };
