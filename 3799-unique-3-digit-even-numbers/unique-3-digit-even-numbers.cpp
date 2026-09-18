b9e7750b641a64287f6e9464dbde4f19c47278c7
Divyanshu-478 <divyanshuaind20@gmail.com>
2026-09-19 01:52:10 +0530

    Time: 10 ms (31.40%) | Memory: 30.9 MB (45.50%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -3,25 +3,14 @@ public:
     int totalNumbers(vector<int>& d) {
         set<int> s;
 
-        for (int i = 0; i < d.size(); i++) {
-            for (int j = 0; j < d.size(); j++) {
-                for (int k = 0; k < d.size(); k++) {
+        for(int i=0;i<d.size();i++)
+            for(int j=0;j<d.size();j++)
+                for(int k=0;k<d.size();k++) {
+                    if(i==j || j==k || i==k) continue;
+                    if(d[i]==0 || d[k]%2) continue;
 
-                    if (i == j || i == k || j == k)
-                        continue;
-
-                    if (d[i] == 0)
-                        continue;
-
-                    if (d[k] % 2 != 0)
-                        continue;
-
-                    int num = d[i] * 100 + d[j] * 10 + d[k];
-
-                    s.insert(num);
+                    s.insert(d[i]*100 + d[j]*10 + d[k]);
                 }
-            }
-        }
 
         return s.size();
     }
