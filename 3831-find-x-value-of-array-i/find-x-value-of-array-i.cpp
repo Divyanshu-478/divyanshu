425b82fd4d77f41be2f66b244361e8afbd0c9273
Divyanshu-478 <divyanshuaind20@gmail.com>
2026-09-21 22:04:20 +0530

    Time: 159 ms (61.61%) | Memory: 151.2 MB (36.61%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,23 +1,19 @@
-int freq[5], freq2[5];
 class Solution {
 public:
-    static vector<long long> resultArray(vector<int>& nums, int k) {
-        const int n=nums.size();
-        if (k==1) return {1LL*n*(n+1)/2};// special case
-        vector<long long> ans(k, 0);
-        memset(freq, 0, sizeof(int)*k);// freq[r]=how many times seen for x%k
-        for (int x: nums){
-            const int r=x%k;
-            memset(freq2, 0, sizeof(int)*k);
-            ans[r]++;
-            for (int j=0; j<k; j++){
-                const int prod=j*r%k;
-                freq2[prod]+=freq[j];
-                ans[prod]+=freq[j];
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> result(k), state(k);
+        for (int value : nums) {
+            int rem = value % k;
+            vector<long long> next(k);
+            for (int r = 0; r < k; r++) {
+                int productRem = (r * rem) % k;
+                next[productRem] += state[r];
+                result[productRem] += state[r];
             }
-            freq2[r]++;
-            memcpy(freq,freq2, sizeof(int)*k);
+            next[rem]++;
+            result[rem]++;
+            state = next;
         }
-        return ans;
+        return result;
     }
 };
